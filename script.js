79a9e95bab0bcbf013a79817ef713cd866519b84
sir wolde <wolderapoo@gmail.com>
2025-11-20 18:49:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded",()=>{
+  const faders=document.querySelectorAll('.fade-in');
+  const appearOptions={threshold:0.1,rootMargin:"0px 0px -50px 0px"};
+  const appearOnScroll=new IntersectionObserver(function(entries,observer){
+    entries.forEach(entry=>{
+      if(!entry.isIntersecting){return;} 
+      else {entry.classList.add('appear'); observer.unobserve(entry);}
+    });
+  },appearOptions);
+  faders.forEach(fader=>{appearOnScroll.observe(fader)});
+});
